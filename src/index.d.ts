2158c9411730b84f3e725a82cbf7447c5f5b5ada
Hossein Mohammadi <hosseinm.developer@gmail.com>
2022-05-25 09:47:24 +0430

    add typescript declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+type MAFunction = (data: number[], size: number) => number[];
+declare const dma: MAFunction;
+declare const sma: MAFunction;
+declare const ema: MAFunction;
+declare const ma: MAFunction;
+declare const wma: MAFunction;
+export { dma, sma, ema, ma, wma };
